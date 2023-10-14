453d231468c07707f890e8c7ce025916831b82dc
Calvin K <calvinknaub@gmail.com>
2023-10-14 11:40:00 -0700

    created my useEffect

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import "./App.css";
-import { connect } from "react-redux";
 import GifList from "./components/GifList";
 import GifForm from "./components/GifForm";
+import { connect } from "react-redux";
+import { useEffect } from "react";
+import { fetchstart, fetchSuccess } from "./actions";
+import axios from "axios";
 
 function App(props) {
   const { loading, error } = props;
+
+  useEffect(() => {
+    props.fetchstart();
+    axios
+      .get(
+        `https://api.giphy.com/v1/gifs/search?api_key=zbJLj05GcKTx4iYyJSVZUUe8p50Mn2WB&q=dogs`
+      )
+      .then((res) => {
+        props.fetchSuccess(res.data.data);
+      });
+  }, []);
 
   return (
     <div className="App">
       <h1>Search for Gifs</h1>
       <GifForm />
 
-      {
-        (error !== "") && <h3>{error}</h3>
-      }
+      {error !== "" && <h3>{error}</h3>}
 
       {loading ? <h3>We are loading</h3> : <GifList />}
     </div>
@@ -27,4 +39,10 @@ const mapStateToProps = (state) => {
   };
 };
 
-export default connect(mapStateToProps, {})(App);
+// const mapActionsToProps = () => {
+//   return {
+//     fetchstart
+//   }
+// }
+
+export default connect(mapStateToProps, { fetchstart, fetchSuccess })(App);
--- src/index.js
+++ src/index.js
@@ -15,8 +15,8 @@ const root = createRoot(document.getElementById("root"));
 
 root.render(
   <Provider store={store}>
-    <React.StrictMode>
+    {/* <React.StrictMode> */}
       <App />
-    </React.StrictMode>
+    {/* </React.StrictMode> */}
   </Provider>
 );
